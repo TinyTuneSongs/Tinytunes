698f5d717e6fb9408f3c4e684ab7090861c553f9
TinyTuneSongs <keaton.davis3@gmail.com>
2026-02-01 01:07:23 +0000

    Add root layout

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Tiny Tunes",
+  description: "Personalized sing-along songs made just for your child."
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
